db84ac0e6850ec7ba435a2b24757f465e27af99c
kcg <cjfrbs1256@gmail.com>
2024-06-21 16:13:58 +0900

    feat: zustand로 toggle버튼및 스타일

--- src/component/MovieList.tsx
+++ src/component/MovieList.tsx
@@ -6,6 +6,11 @@ import './movie.css';
 
 const MovieList: React.FC = () => {
   const [page, setPage] = useState(1);
+  // const [selectCategory, setSelectCategory] = useState(true);
+  // const toggleButton = (isToggle: boolean) => {
+  //   setSelectCategory(isToggle);
+  // };
+  const { isToggle, toggleCategory } = useMovieStore();
   const {
     data: movies,
     isLoading: mIsLoading,
@@ -19,6 +24,9 @@ const MovieList: React.FC = () => {
     isFetching: tIsFetching
   } = useTv(page);
   const addFavorite = useMovieStore((state) => state.addFavorite);
+  const favoriteMovies = useMovieStore((state) => state.favoriteMovies);
+  console.log(addFavorite);
+  console.log(favoriteMovies);
 
   if (mIsLoading || mIsFetching || tIsLoading || tIsFetching)
     return <div>Loading...</div>;
@@ -27,40 +35,46 @@ const MovieList: React.FC = () => {
   return (
     <div className='all'>
       <div>
-        <h2>All Movies</h2>
-        <ul>
-          {movies?.map((movie: Movie) => (
-            <li key={movie.id}>
-              {movie.title}
-              <img
-                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-              />
-              <button onClick={() => addFavorite(movie.id)}>
-                Add to Favorites
-              </button>
-            </li>
-          ))}
-        </ul>
+        <span
+          className={`toggleBtn ${isToggle ? 'active' : ''}`}
+          onClick={toggleCategory}>
+          Movie
+        </span>
+        <span
+          className={`toggleBtn ${!isToggle ? 'active' : ''}`}
+          onClick={toggleCategory}>
+          TV
+        </span>
+      </div>
+      {isToggle &&
+        movies?.map((movie: Movie) => (
+          <li key={movie.id}>
+            {movie.title}
+            <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} />
+            <button onClick={() => addFavorite(movie.id)}>
+              Add to Favorites
+            </button>
+          </li>
+        ))}
+      {/* <div>
         <div>
           <button onClick={() => setPage((prev) => Math.max(prev - 1, 1))}>
             Previous
           </button>
           <button onClick={() => setPage((prev) => prev + 1)}>Next</button>
         </div>
-      </div>
+      </div> */}
+      {!isToggle &&
+        tvshows?.map((tv: TV) => (
+          <li key={tv.id}>
+            {tv.name}
+            <img src={`https://image.tmdb.org/t/p/w200${tv.poster_path}`} />
+            <button onClick={() => addFavorite(tv.id)}>Add to Favorites</button>
+          </li>
+        ))}
       <div>
         <h2>All TV</h2>
-        <ul>
-          {tvshows?.map((tv: TV) => (
-            <li key={tv.id}>
-              {tv.name}
-              <img src={`https://image.tmdb.org/t/p/w200${tv.poster_path}`} />
-              <button onClick={() => addFavorite(tv.id)}>
-                Add to Favorites
-              </button>
-            </li>
-          ))}
-        </ul>
+        <ul></ul>
         <div>
           <button onClick={() => setPage((prev) => Math.max(prev - 1, 1))}>
             Previous
--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,15 +1,23 @@
 import create from 'zustand';
 
-interface MovieStore {
+type MovieStore = {
   favoriteMovies: number[];
   addFavorite: (id: number) => void;
   removeFavorite: (id: number) => void;
-}
+  isToggle: boolean;
+  toggleCategory: () => void;
+};
 
 const useMovieStore = create<MovieStore>((set) => ({
   favoriteMovies: [],
-  addFavorite: (id) => set((state) => ({ favoriteMovies: [...state.favoriteMovies, id] })),
-  removeFavorite: (id) => set((state) => ({ favoriteMovies: state.favoriteMovies.filter((movieId) => movieId !== id) })),
+  addFavorite: (id) =>
+    set((state) => ({ favoriteMovies: [...state.favoriteMovies, id] })),
+  removeFavorite: (id) =>
+    set((state) => ({
+      favoriteMovies: state.favoriteMovies.filter((movieId) => movieId !== id)
+    })),
+  isToggle: true,
+  toggleCategory: () => set((state) => ({ isToggle: !state.isToggle }))
 }));
 
 export default useMovieStore;
